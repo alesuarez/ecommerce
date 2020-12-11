387680aa371bc8a80413f8c7118acfcd6ba8f655
Alejandro Suarez <sas.tuc@gmail.com>
2020-12-11 07:50:10 -0300

    Add cartItem functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
+
 import usesStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
 
     const classes = usesStyles();
-    const isEmpty = cart.length === 0;
 
     const EmptyCart = () => (
         <Typography variant="subtitle1"> No tienes ningun producto en tu carrito de compras </Typography>
@@ -16,25 +17,26 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}> 
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}> 
-                        <div> {item.name} </div>                    
+                        <CartItem item={item}/>
                     </Grid>
                 ))}
             </Grid>
             <div className={classes.CardDetails}>
                 <Typography variant="h4"> Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Carrito vacio</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Vaciar carrito</Button>
                     <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Comprar</Button>
                 </div>
             </div>
         </>
     )
 
+    if (!cart.line_items) return 'Loading...'
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3"> Tu carrito de compras</Typography>
-            { isEmpty ? <EmptyCart /> : <FilledCart /> }  
+            <Typography className={classes.title} variant="h3" gutterBottom> Tu carrito de compras</Typography>
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }  
         </Container>
     )
 }
